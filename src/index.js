cbde4803c11cd0c7ae29982283b2a8e39ad31565
jessesbyers <jesse.s.byers@gmail.com>
2020-04-07 15:30:28 -0400

    installed react-redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import manageStudentInput from './reducers/manageStudentInput'
+
+
+// const store = createStore(manageStudentInput)
+
+
 ReactDOM.render(
+  // <Provider store={store}>
     <App />,
+    // </Provider>,
   document.getElementById('root')
 );
 
