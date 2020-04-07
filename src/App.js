3ffd3578ec378fd267fce77b95c7ecc4a5dd3b6f
jessesbyers <jesse.s.byers@gmail.com>
2020-04-07 13:50:38 -0400

    tested fetch from rails api and external api in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-class App extends React.component {
+class App extends Component {
+
+  componentDidMount() {
+  // test fetch from rails api
+
+    fetch(`http://localhost:3000/api/v1/charts`)
+    .then(response => response.json())
+    .then(json => console.log(json))
+    
+  // test fetch from external api
+    fetch('https://www.ncdc.noaa.gov/cag/national/time-series/110-tavg-12-12-1895-2020.json?base_prd=true&begbaseyear=1930&endbaseyear=2000')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
 
   render() {
     return (
