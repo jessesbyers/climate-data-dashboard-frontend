2371413d008547e7bd91a25f5e7bdf5dbccdfabf
jessesbyers <jesse.s.byers@gmail.com>
2020-04-07 19:55:21 -0400

    added react-bootstrap imports in index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './containers/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
