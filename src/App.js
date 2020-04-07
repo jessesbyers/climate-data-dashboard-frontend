a9eb632e810030943dd40a32bc7e6a93d6bea970
jessesbyers <jesse.s.byers@gmail.com>
2020-04-07 14:33:59 -0400

    tested fetch from rails api with serialized data

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
   componentDidMount() {
   // test fetch from rails api
 
-    fetch(`http://localhost:3000/api/v1/charts`)
+    fetch(`http://localhost:3000/charts`)
     .then(response => response.json())
     .then(json => console.log(json))
-    
+
   // test fetch from external api
     fetch('https://www.ncdc.noaa.gov/cag/national/time-series/110-tavg-12-12-1895-2020.json?base_prd=true&begbaseyear=1930&endbaseyear=2000')
     .then(response => response.json())
