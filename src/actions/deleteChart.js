89de17c6f3e6d8e9146f0bd747af0b8031a0526d
jessesbyers <jesse.s.byers@gmail.com>
2020-04-08 15:15:10 -0400

    added comments to actions files

--- src/actions/deleteChart.js
+++ src/actions/deleteChart.js
@@ -1,3 +1,6 @@
+
+// action removes chart from state so it is not renderIntoDocument, 
+// but chart is still stored in database
 export const deleteChart = (id) => {
     return {
         type: 'DELETE_CHART',
--- src/actions/fetchClimateData.js
+++ src/actions/fetchClimateData.js
@@ -1,4 +1,7 @@
-  //   // test fetch from external api
+// action will fetch json data from external API for D3 data visualizations (later) 
+ 
+ 
+ //   // test fetch from external api
   //       fetch('https://www.ncdc.noaa.gov/cag/national/time-series/110-tavg-12-12-1895-2020.json?base_prd=true&begbaseyear=1930&endbaseyear=2000')
   //       .then(response => response.json())
   //       .then(json => console.log(json))
