7fdc384bb61dd8ca2d6c3731a42a56436bad0369
jessesbyers <jesse.s.byers@gmail.com>
2020-04-08 14:35:56 -0400

    revised diabled option on select dropdown in DropdownInput.js

--- src/components/DropdownInput.js
+++ src/components/DropdownInput.js
@@ -15,7 +15,7 @@ class DropdownInput extends Component {
 
                 <form onSubmit={event => this.props.handleDropdownSubmit(event)}>
                     <select value={this.state.chart_url} onChange={event => this.props.handleDropdownChange(event)}>
-                        <option value="" disabled selected>Choose a Chart to Add to the Data Dashboard</option>
+                        <option value="" disabled>Choose a Chart to Add to the Data Dashboard</option>
                         <option value="http://localhost:3000/charts/1">Contiguous U.S., Average Temperature, January-December</option>
                         <option value="http://localhost:3000/charts/2">Contiguous U.S., Precipitation, January-December</option>
                         <option value="http://localhost:3000/charts/3">Vermont, Average Temperature, January-December</option>
