2b4272355263b921f3250a31e743528d30854fdd
jessesbyers <jesse.s.byers@gmail.com>
2020-04-14 15:08:05 -0400

    manipulated local state in DropdownInput.js to control dropdown options

--- src/components/DropdownInput.js
+++ src/components/DropdownInput.js
@@ -4,26 +4,22 @@ import { deleteChart } from '../actions/deleteChart'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-
-
-
 import { connect } from 'react-redux'
 
 class DropdownInput extends Component {
 
     state = {
         chart_url: "",
-        charts: []
+        charts: [],
+        removedCharts: []
     }
 
     componentDidMount(){
         fetch("http://localhost:3000/charts")
         .then(response => response.json())
         .then(charts => {
-          console.log(charts)
           this.setState({charts})
         })
-
     }
 
     handleDropdownChange = (event) => {
@@ -31,28 +27,43 @@ class DropdownInput extends Component {
     }
 
     handleDropdownSubmit = (event) => {
+        console.log(this.state.charts)
         event.preventDefault()
+
         if (this.chart_url !== "") {
             this.props.fetchChartData(this.state.chart_url)
             this.props.fetchNotices(this.state.chart_url)
             this.props.fetchWonders(this.state.chart_url)
+
+            // manipulating local state to remove from dropdown any charts already in the dashboard
+            this.setState({
+                charts: this.state.charts.filter(chart => this.state.chart_url !== `http://localhost:3000/charts/${chart.id}`),
+                removedCharts: this.state.charts.filter(chart => this.state.chart_url === `http://localhost:3000/charts/${chart.id}`)
+            })
         } else {
             return "Please choose a chart from the dropdown menu"
         }
     }
 
     handleDelete = () => {
-        console.log(this)
-        this.props.charts.map(chart => this.props.deleteChart(chart))
+        this.props.charts.map(chart => {
+            this.props.deleteChart(chart)
+        })
+        // manipulating local state to render all chart options in dropdown menu
+        this.setState({
+            charts: [...this.state.charts.concat(this.props.charts)],
+            removedCharts: []
+        })
     }
 
     render() {    
-        console.log(this.state)
+        console.log(this.props.charts)
+
         return (
             <Form onSubmit={event => this.handleDropdownSubmit(event)}>
                 <Form.Group>
                     <Form.Control as="select" size="lg" value={this.state.chart_url} onChange={event => this.handleDropdownChange(event)}>
-                        <option value="" disabled>Choose a Chart to Load to the Data Dashboard</option>
+                        <option  value="" >Choose a Chart to Load to the Data Dashboard</option>
                         {this.state.charts.map(chart => <option value={`http://localhost:3000/charts/${chart.id}`} key={chart.id}>{chart.name}</option>)}
                     </Form.Control>
                 </Form.Group>
@@ -78,3 +89,4 @@ const mapStateToProps = (state) => {
 }
   
 export default connect (mapStateToProps, { fetchChartData, fetchNotices, fetchWonders, deleteChart} )(DropdownInput);
+
