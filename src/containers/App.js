3f9c4c8c71ae0c3bc6d232969590738528c33abc
jessesbyers <jesse.s.byers@gmail.com>
2020-04-15 11:19:11 -0400

    added comments to App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,26 @@
-// component provides built-in methods and attributes
+// component provides built-in methods and attributes for components, including lifecycle methods, render(), constructor()
 import React, { Component } from 'react';
 
-// imports custom css styling
+// imports custom css styling for App.js file
 import '../App.css';
+
+// imports BrowserRouter component which allows us to create multiple routes form the single index.html page
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// enables these local container/component files to be rendered through the routing options
 import NavBar from '../components/NavBar';
 import Home from '../containers/Home'
 import New from '../containers/New'
 import Show from '../containers/Show'
 import DataSourceInput from '../components/DataSourceInput';
 
+// imports react-bootstrap container, which is set as fluid to be responsive
 import Container from 'react-bootstrap/Container';
 
-
+// The container and navbar will be rendered on every page, while the other components are rendered based on the url
 class App extends Component {
 
   render() {
-
     return (
       <Router >
         <Container fluid >
