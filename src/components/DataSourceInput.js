ae232db73088e27c004bb938ce3094d4cda3fba0
jessesbyers <jesse.s.byers@gmail.com>
2020-04-15 13:17:58 -0400

    comments added to DataSourceInput and DropdownInput

--- src/components/DataSourceInput.js
+++ src/components/DataSourceInput.js
@@ -1,15 +1,25 @@
+// component provides built-in methods and attributes for components, including lifecycle methods, render(), constructor()
 import React, { Component } from 'react';
+
+// importing actions from local files
 import { addDataSource} from '../actions/addDataSource'
-import { connect } from 'react-redux'
+
+// importing components from local files
 import Chart from '../components/Chart'
 
+// bootstrap components for css styling
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 
+// enables this component to access state data from the redux store if component is wrapped with connect
+import { connect } from 'react-redux'
+
 
 
 class DataSourceInput extends Component {
+    // initial state has placeholders for form input, 
+    // as well as key to direct what content is visible in the render function based on user actions
     state = {
         name: "",
         data_url: "",
@@ -17,17 +27,21 @@ class DataSourceInput extends Component {
         visible: false
     }
 
+    // state is updated every time the form value changes. Default values for chart_id and votes are assigned.
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         })
     }
 
+    // the visible key is reset to true to show a confirmation message before calling the fetch action.
     handleSubmit = (event) => {
         event.preventDefault()
         this.setState({ visible: true})
     }
 
+    // The addDataSource action is called, which triggers the POST fetch request for the new chart
+    // state is reset to initial values
     handleSave = () => {
         this.props.addDataSource({name: this.state.name, data_url: this.state.data_url, screenshot_url: this.state.screenshot_url})
         this.setState({
@@ -39,10 +53,12 @@ class DataSourceInput extends Component {
 
     }
 
+    // state.visible is changed to true to show confirmation prompt
     handleClick = () => {
         this.setState({ visible: true})
     }
 
+    // state is reset to initial values without sending the post fetch request
     handleCancel = () => {
         this.setState({
             name: "",
@@ -53,9 +69,10 @@ class DataSourceInput extends Component {
     }
 
     render() {
-        console.log(this)
+        // conditional logic looks to values in local state to determine which content to return
 
         if (this.state.visible === false) {
+            // renders form to input new data source
             return (
                 <Card>
                     <Card.Header>Add a New Data Source</Card.Header>
@@ -80,6 +97,7 @@ class DataSourceInput extends Component {
             )
         } 
         else {
+            // returns draft of input data from local state and asks for confirmation before sending fetch request 
             return (
                 <Card>
                     <Card.Header>Confirm your New Data Source</Card.Header>
@@ -92,13 +110,8 @@ class DataSourceInput extends Component {
             )
         }        
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-      state
-    }
-}
-
-export default connect (mapStateToProps, {addDataSource})(DataSourceInput)
+// nothing needs to be added to props for this component
+// shorthand for mapDispatchToProps for addDataSource action
+export default connect (null, {addDataSource})(DataSourceInput)
